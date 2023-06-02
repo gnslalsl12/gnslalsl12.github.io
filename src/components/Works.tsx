097c7ef8fa8bccc1af1f583e1d77bf2bafbc7e5a
gnslalsl12 <wjdgnsxhsl@naver.com>
2023-06-02 13:11:11 +0900

    [FEAT] edit Font to E-Eum Project on Pretendard

--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import { BsBoxArrowUpRight } from 'react-icons/bs';
 import project2D_Worldy from '../images/project2D_Worldy.png';
+import project2D_Eeum from '../images/project2D_E-Eum.png';
+import project2D_RendezBoo from '../images/project2D_RendezBoo.png';
 
 type WorksItemType = {
   projectTitle: string;
@@ -18,10 +20,32 @@ const Works = ({ windowWidth }: { windowWidth: number }): JSX.Element => {
   const ProjectList: WorksItemType[] = [
     {
       projectTitle: 'Worldy',
-      projectType: '팀 프로젝트',
+      projectType: '팀 프로젝트 (웹)',
       projectExplain:
         '월디게임 어쩌구저쩌구 설명 어쩌구적ㅇ너미소ㅓㄴ이ㅗ서니소ㅓㅁ나솸놋니ㅗ서ㅣ나ㅚ',
       project2DSrc: project2D_Worldy,
+      project3DSrc: '../images/',
+      projectLink: 'https://gnslalsl12.github/~',
+      myAfford: ['페이지 구성', '게임 디자인', '페이지 알고리즘'],
+      projectSkills: ['TypeScript', 'React', 'Three.js'],
+    },
+    {
+      projectTitle: '이음',
+      projectType: '팀 프로젝트 (모바일)',
+      projectExplain:
+        '월디게임 어쩌구저쩌구 설명 어쩌구적ㅇ너미소ㅓㄴ이ㅗ서니소ㅓㅁ나솸놋니ㅗ서ㅣ나ㅚ',
+      project2DSrc: project2D_Eeum,
+      project3DSrc: '../images/',
+      projectLink: 'https://gnslalsl12.github/~',
+      myAfford: ['페이지 구성', '게임 디자인', '페이지 알고리즘'],
+      projectSkills: ['TypeScript', 'React', 'Three.js'],
+    },
+    {
+      projectTitle: 'Rendez-Boo',
+      projectType: '팀 프로젝트 (웹)',
+      projectExplain:
+        '월디게임 어쩌구저쩌구 설명 어쩌구적ㅇ너미소ㅓㄴ이ㅗ서니소ㅓㅁ나솸놋니ㅗ서ㅣ나ㅚ',
+      project2DSrc: project2D_RendezBoo,
       project3DSrc: '../images/',
       projectLink: 'https://gnslalsl12.github/~',
       myAfford: ['페이지 구성', '게임 디자인', '페이지 알고리즘'],
@@ -38,7 +62,15 @@ const Works = ({ windowWidth }: { windowWidth: number }): JSX.Element => {
           <span className='project_type'>
             {ProjectList[selectedIndex].projectType}
           </span>
-          <span className='project_title'>
+          <span
+            className='project_title'
+            style={{
+              fontFamily:
+                selectedIndex === 1
+                  ? 'Pretendard-ExtraBold'
+                  : 'Montserrat-ExtraBold',
+            }}
+          >
             {ProjectList[selectedIndex].projectTitle}
           </span>
           <span className='project_explain'>
