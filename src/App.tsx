330ccf2c7bf3fab7e483cd8af8f02481ad9fc6f0
정훈 <wjdgnsxhsl@naver.com>
2023-06-09 14:53:00 +0900

    [FIX] edit Works Pages with Designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,79 +1,79 @@
-import * as React from 'react';
-import { useState, useEffect, useRef } from 'react';
+import * as React from "react";
+import { useState, useEffect, useRef } from "react";
 
-import NavigationBar from './components/NavigationBar';
-import Main from './components/Main';
-import About from './components/About';
-import Works from './components/Works';
+import NavigationBar from "./components/NavigationBar";
+import Main from "./components/Main";
+import About from "./components/About";
+import Works from "./components/Works";
 
 function App() {
   const [windowWidth, setWindowWidth] = useState<number>(0);
-
-  const topRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (topRef.current) {
-      topRef.current.scrollIntoView({ behavior: 'auto', block: 'start' });
-    }
-  }, []);
 
   useEffect(() => {
     if (windowWidth === 0) setWindowWidth(window.innerWidth);
   }, [windowWidth]);
 
-  useEffect(() => {
-    // if (windowWidth === undefined) setWindowWidth(window.innerWidth);
+  // const topRef = useRef<HTMLDivElement>(null);
 
-    const handleResizeWindowWidth = () => {
-      setWindowWidth(window.innerWidth);
-    };
+  // useEffect(() => {
+  //   if (topRef.current) {
+  //     topRef.current.scrollIntoView({ behavior: 'auto', block: 'start' });
+  //   }
+  // }, []);
 
-    window.addEventListener('resize', handleResizeWindowWidth);
+  // useEffect(() => {
+  //   // if (windowWidth === undefined) setWindowWidth(window.innerWidth);
 
-    return () => {
-      //컴포넌트 언마운트 시 리스너 제거용
-      window.removeEventListener('resize', handleResizeWindowWidth);
-    };
-  }, []);
+  //   const handleResizeWindowWidth = () => {
+  //     setWindowWidth(window.innerWidth);
+  //   };
 
-  useEffect(() => {
-    const handleScroll = (e: WheelEvent) => {
-      e.preventDefault();
+  //   window.addEventListener('resize', handleResizeWindowWidth);
 
-      const delta = e.deltaY;
-      const scrollAmount = window.innerHeight / 3;
+  //   return () => {
+  //     //컴포넌트 언마운트 시 리스너 제거용
+  //     window.removeEventListener('resize', handleResizeWindowWidth);
+  //   };
+  // }, []);
 
-      if (delta < 0) {
-        window.scroll({
-          top: window.pageYOffset - scrollAmount,
-          behavior: 'smooth',
-        });
-      } else {
-        window.scroll({
-          top: window.pageYOffset + scrollAmount,
-          behavior: 'smooth',
-        });
-      }
-    };
+  // useEffect(() => {
+  //   const handleScroll = (e: WheelEvent) => {
+  //     e.preventDefault();
 
-    window.addEventListener('wheel', handleScroll, { passive: false });
+  //     const delta = e.deltaY;
+  //     const scrollAmount = window.innerHeight / 3;
 
-    return () => {
-      window.removeEventListener('wheel', handleScroll);
-    };
-  }, []);
+  //     if (delta < 0) {
+  //       window.scroll({
+  //         top: window.pageYOffset - scrollAmount,
+  //         behavior: 'smooth',
+  //       });
+  //     } else {
+  //       window.scroll({
+  //         top: window.pageYOffset + scrollAmount,
+  //         behavior: 'smooth',
+  //       });
+  //     }
+  //   };
 
-  const [loadOtherPages, setLoadOtherPages] = useState<boolean>(false);
+  //   window.addEventListener('wheel', handleScroll, { passive: false });
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoadOtherPages(true);
-    }, 2000);
-  }, []);
+  //   return () => {
+  //     window.removeEventListener('wheel', handleScroll);
+  //   };
+  // }, []);
+
+  const [loadOtherPages, setLoadOtherPages] = useState<boolean>(true);
+
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoadOtherPages(true);
+  //   }, 2000);
+  // }, []);
 
   return (
-    <div id='page_index' ref={topRef}>
-      <div id='page_index_background' />
+    <div id="page_index">
+      <div id="page_index_background" />
       <NavigationBar windowWidth={windowWidth} />
       <Main windowWidth={windowWidth} />
       {loadOtherPages && (
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -1,20 +1,18 @@
-import * as React from 'react';
-import { useState, useEffect } from 'react';
-import { BsBoxArrowUpRight } from 'react-icons/bs';
-import { BsArrowLeft, BsArrowRight } from 'react-icons/bs';
-import project2D_Worldy from '../images/project2D_Worldy.png';
-import project2D_Eeum from '../images/project2D_E-Eum.png';
-import project2D_RendezBoo from '../images/project2D_RendezBoo.png';
-import project3D_Worldy from '../images/project3D_Worldy.png';
-import project3D_Eeum from '../images/project3D_E-Eum.png';
-import project3D_RendezBoo from '../images/project3D_RendezBoo.png';
-import Logo_Worldy from '../images/Logo_Worldy.png';
-import Logo_Eeum from '../images/Logo_Eeum.png';
-import Logo_RendezBoo from '../images/Logo_RendezBoo.png';
-
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import * as React from "react";
+import { useState, useEffect } from "react";
+import { SiGithub } from "react-icons/si";
+import project2D_Worldy from "../images/project2D_Worldy.png";
+import project2D_Eeum from "../images/project2D_E-Eum.png";
+import project2D_RendezBoo from "../images/project2D_RendezBoo.png";
+import project3D_Worldy from "../images/project3D_Worldy.png";
+import project3D_Eeum from "../images/project3D_E-Eum.png";
+import project3D_RendezBoo from "../images/project3D_RendezBoo.png";
+import Logo_Worldy from "../images/Logo_Worldy.png";
+import Logo_Eeum from "../images/Logo_Eeum.png";
+import Logo_RendezBoo from "../images/Logo_RendezBoo.png";
+import project_Worldy_Intro from "../images/project_Worldy_Intro.png";
+import project_Eeum_Intro from "../images/project_Eeum_Intro.png";
+import project_RendezBoo_Intro from "../images/project_RendezBoo_Intro.png";
 
 type WorksItemType = {
   type: string;
@@ -23,102 +21,143 @@ type WorksItemType = {
   about_title: string;
   about_explain: string;
   link: string;
+  intro: string;
   img_2d: string;
   img_3d: string;
   logo: string;
 };
 
+type Projects = {
+  [key: string]: WorksItemType;
+};
+
 const Works = ({ windowWidth }: { windowWidth: number }): JSX.Element => {
-  const WorksItemList: WorksItemType[] = [
-    {
-      type: 'Team Project (Web)',
-      title: 'Worldy',
-      period: '2023.04~2023.05',
-      about_title: 'Worldy 프로젝트 주제',
-      about_explain: 'Worldy 프로젝트 소개',
-      link: 'https://',
+  const WorksItemList: Projects = {
+    Worldy: {
+      type: "Team Project (Web)",
+      title: "Worldy",
+      period: "2023.04~2023.05",
+      about_title: "Worldy 프로젝트 주제",
+      about_explain: "Worldy 프로젝트 소개",
+      link: "https://",
+      intro: project_Worldy_Intro,
       img_2d: project2D_Worldy,
       img_3d: project3D_Worldy,
       logo: Logo_Worldy,
     },
-    {
-      type: 'Team Project (Mobile)',
-      title: 'E-Eum',
-      period: '2023.02~2023.04',
-      about_title: '이음 프로젝트 주제',
-      about_explain: '이음 프로젝트 소개',
-      link: 'https://',
+    Eeum: {
+      type: "Team Project (Mobile)",
+      title: "E-Eum",
+      period: "2023.02~2023.04",
+      about_title: "이음 프로젝트 주제",
+      about_explain: "이음 프로젝트 소개",
+      link: "https://",
+      intro: project_Eeum_Intro,
       img_2d: project2D_Eeum,
       img_3d: project3D_Eeum,
       logo: Logo_Eeum,
     },
-    {
-      type: 'Team Project (WEB)',
-      title: 'Rendez-BOO',
-      period: '2023.01~2023.02',
-      about_title: '랑데부 프로젝트 주제',
-      about_explain: '랑데부 프로젝트 소개',
-      link: 'https://',
+    RendezBoo: {
+      type: "Team Project (WEB)",
+      title: "Rendez-BOO",
+      period: "2023.01~2023.02",
+      about_title: "랑데부 프로젝트 주제",
+      about_explain: "랑데부 프로젝트 소개",
+      link: "https://",
+      intro: project_RendezBoo_Intro,
       img_2d: project2D_RendezBoo,
       img_3d: project3D_RendezBoo,
       logo: Logo_RendezBoo,
     },
-    {
-      type: 'Team Project (WEB)',
-      title: 'Rendez-BOO',
-      period: '2023.01~2023.02',
-      about_title: '랑데부 프로젝트 주제',
-      about_explain: '랑데부 프로젝트 소개',
-      link: 'https://',
-      img_2d: project2D_RendezBoo,
-      img_3d: project3D_RendezBoo,
-      logo: Logo_RendezBoo,
-    },
-  ];
+  };
 
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: false,
-    draggable: false,
-    pauseOnHover: true,
-    swipe: false,
-    touchMove: false,
-    // rtl: true,
+  const [detailShowState, setDetailShowState] = useState<boolean>(false);
+  const [detailProject, setDetailProject] = useState<string>("");
+
+  const DetailModal = (): JSX.Element => {
+    return <div></div>;
   };
 
   return (
-    <div id='page_works'>
-      <div id='works_container'>
-        <div className='works_flows_box'></div>
-        <div className='works_menu_box'>
-          <div className='works_menu_top'></div>
-          <div className='works_menu_bottom'>
-            <Slider {...settings}>
-              <div>
-                <h3>Slide 6</h3>
+    <div id="page_works">
+      {detailProject !== "" && <DetailModal />}
+      <div className="works_container">
+        <div className="works_top_box">
+          <span className="works_top_text">Works</span>
+        </div>
+        <div className="works_bottom_box">
+          <div className="bottom_box_flows">
+            <div
+              id="work_box_Worldy"
+              className="work_box_web"
+              onClick={() => {
+                setDetailProject("Worldy");
+                setDetailShowState(true);
+              }}
+            >
+              <div className="work_text_box">
+                <span>Worldy</span>
               </div>
-              <div>
-                <h3>Slide 1</h3>
+              <div className="work_image_box">
+                <img src={WorksItemList.Worldy.intro} alt="intro" />
               </div>
-              <div>
-                <h3>Slide 2</h3>
+            </div>
+            <div
+              id="work_box_RendezBoo"
+              className="work_box_web"
+              onClick={() => {
+                setDetailProject("RendezBoo");
+                setDetailShowState(true);
+              }}
+            >
+              <div className="work_text_box">
+                <span>
+                  Rendez
+                  <br />
+                  Boo
+                </span>
               </div>
-              <div>
-                <h3>Slide 3</h3>
+              <div className="work_image_box">
+                <img src={WorksItemList.RendezBoo.intro} alt="intro" />
               </div>
-              <div>
-                <h3>Slide 4</h3>
+            </div>
+            <div id="work_box_Olds">
+              <div
+                id="work_box_MyHome"
+                className="work_box"
+                onClick={() => {
+                  setDetailProject("MyHome");
+                  setDetailShowState(true);
+                }}
+              >
+                <div className="work_text_box">
+                  <span>
+                    Where is
+                    <br />
+                    My Home
+                  </span>
+                </div>
+                <div className="work_image_box"></div>
               </div>
-              <div>
-                <h3>Slide 5</h3>
+              <a id="work_box_More" className="work_box" href="https://github.com/gnslalsl12">
+                <SiGithub color="white" />
+              </a>
+            </div>
+            <div
+              id="work_box_Eeum"
+              className="work_box_mobile"
+              onClick={() => {
+                setDetailProject("Eeum");
+                setDetailShowState(true);
+              }}
+            >
+              <div className="work_text_box">
+                <span>이음</span>
               </div>
-            </Slider>
+              <div className="work_image_box">
+                <img src={WorksItemList.Eeum.intro} alt="intro" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
