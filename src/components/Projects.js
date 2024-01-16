ae559ffb0f72f557614f47fac91c4fe5f37b607f
정훈 <wjdgnsxhsl@naver.com>
2024-01-16 16:23:20 +0900

    add project contents

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import Worldy_Overview from "../assets/images/projects/Worldy_Overview.png";
+import Eeum_Overview from "../assets/images/projects/Eeum_Overview.png";
+import Rendez_Overview from "../assets/images/projects/Rendez_Overview.png";
 
 const Projects = () => {
   const [filtered, setFiltered] = useState(0);
@@ -17,18 +20,42 @@ const Projects = () => {
 
   const projectsCardList = () => {
     const projects = [
-      { title: "WORLDY", period: "2023-04-10 ~ 2023-05-26", imgSrc: "" },
-      { title: "이음", period: "2023-02-20 ~ 2023-04-07", imgSrc: "" },
-      { title: "Rendez-Boo", period: "2023-01-03 ~ 2023-02-17", imgSrc: "" },
+      {
+        title: "WORLDY",
+        period: "23-04-10 ~ 23-05-26",
+        imgSrc: Worldy_Overview,
+        skills: "React & Three.js",
+      },
+      {
+        title: "이음",
+        period: "23-02-20 ~ 23-04-07",
+        imgSrc: Eeum_Overview,
+        skills: "ReactNative",
+      },
+      {
+        title: "Rendez-Boo",
+        period: "23-01-03 ~ 23-02-17",
+        imgSrc: Rendez_Overview,
+        skills: "React & WebRTC",
+      },
     ];
 
     return projects.map((value, index) => {
       return (
         <li key={index}>
-          <img src={value.imgSrc}></img>
+          <div className="projects_imgBox">
+            <img src={value.imgSrc}></img>
+          </div>
           <div className="projects_summary">
-            <span className="projects_summary_title">{value.title}</span>
-            <span className="projects_summary_period">{value.period}</span>
+            <div className="projects_summary_topBox">
+              <span className="projects_summary_title">
+                {value.title}
+                <span>{value.skills}</span>
+              </span>
+            </div>
+            <div className="projects_summary_bottomBox">
+              <button className="projects_summary_button">Details</button>
+            </div>
           </div>
         </li>
       );
