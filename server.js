23dac51900282ed5d59930a54892d29139fe0ade
정훈 <wjdgnsxhsl@naver.com>
2024-01-17 23:18:43 +0900

    change gitLink

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+// 정적 파일 제공
+app.use(express.static(path.join(__dirname, "build")));
+
+// 모든 요청을 index.html로 리디렉션
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
