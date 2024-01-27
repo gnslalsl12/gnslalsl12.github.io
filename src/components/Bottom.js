a77dfbd2502accdca68a0c766ab736cc37bdbce8
정훈 <wjdgnsxhsl@naver.com>
2024-01-27 16:36:27 +0900

    add goTopButton

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+import { MdKeyboardDoubleArrowUp } from "react-icons/md";
+import ActiveSectionContext from "../contexts/ActiveSectionContext";
+import ScrollToSectionContext from "../contexts/ScrollToSectionContext";
 
 const Bottom = () => {
-  return <div></div>;
+  const { activeSection, setActiveSection } = useContext(ActiveSectionContext);
+
+  const { scrollToSection } = useContext(ScrollToSectionContext);
+
+  return (
+    <div className="page_bottom_container">
+      <button
+        className="page_bttom_topButton"
+        onClick={() => {
+          setActiveSection("home");
+          scrollToSection("home");
+        }}
+      >
+        <MdKeyboardDoubleArrowUp />
+      </button>
+    </div>
+  );
 };
 export default Bottom;
